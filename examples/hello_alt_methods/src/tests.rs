a8322dd36a8c91318f7dfd9e83523c2af6fa12c8
Robert <robert.balicki@gmail.com>
2016-12-29 20:06:42 -0500

    write tests for hello_alt_methods

---
+++ examples/hello_alt_methods/src/tests.rs
@@ -0,0 +1,31 @@
+use super::rocket;
+use rocket::testing::MockRequest;
+use rocket::http::Method;
+use rocket::http::Status;
+
+#[test]
+fn hello_world_alt_methods() {
+    test(Method::Get, Status::Ok, Some("<!DOCTYPE html>"));
+    test(Method::Put, Status::Ok, Some("Hello, PUT request!"));
+    test(Method::Post, Status::NotFound, None);
+}
+
+fn test(
+    method: Method,
+    expected_status: Status,
+    expected_body_starts_with: Option<&str>
+) {
+    let rocket = rocket::ignite()
+        .mount("/", routes![super::index, super::put]);
+
+    let mut req = MockRequest::new(method, "/");
+    let mut response = req.dispatch_with(&rocket);
+
+    assert_eq!(response.status(), expected_status);
+    expected_body_starts_with.map(|expected_body_string| {
+        let body_str = response.body().and_then(|body| body.into_string()).unwrap();
+        let body_starts_correctly = body_str.starts_with(expected_body_string);
+        assert!(body_starts_correctly);
+    });
+}
+
