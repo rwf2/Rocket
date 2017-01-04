0810d835109c68e1a63b0c34dd3279c5dbda2a8b
Cliff H <cliffhazelton@gmail.com>
2017-01-03 20:56:38 -0500

    Switch argument order (actual, expected) for assert_eq! assertions. Added a match for the header names to perform assertions on their values

--- examples/redirect/src/tests.rs
+++ examples/redirect/src/tests.rs
@@ -17,12 +17,12 @@ macro_rules! run_test {
 fn test_root() {
     run_test!("/", |mut response: Response| {
         assert!(response.body().is_none());
-        assert_eq!(Status::SeeOther, response.status());
-        for h in response.headers(){
-            if h.name == "Location" {
-                assert_eq!("/login", h.value);
-            } else if h.name == "Content-Length" {
-                assert_eq!("0", h.value);
+        assert_eq!(response.status(), Status::SeeOther);
+        for h in response.headers() {
+            match h.name.as_ref() {
+                "Location" => assert_eq!(h.value, "/login"),
+                "Content-Length" => assert_eq!(h.value.parse::<i32>().unwrap(), 0),
+                _ => { /* let these through */ }
             }
         }
     });
@@ -32,7 +32,7 @@ fn test_root() {
 fn test_login() {
     run_test!("/login", |mut response: Response| {
         let body_string = response.body().and_then(|body| body.into_string());
-        assert_eq!(Some("Hi! Please log in before continuing.".to_string()), body_string);
-        assert_eq!(Status::Ok, response.status());
+        assert_eq!(body_string, Some("Hi! Please log in before continuing.".to_string()));
+        assert_eq!(response.status(), Status::Ok);
     });
 }
