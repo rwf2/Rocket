a777a5cf161af880d64b3497447318cdc72227a7
Lori Holden <email@loriholden.com>
2017-01-20 13:25:56 -0500

    Simplify to the one example handler

--- examples/uuid/src/main.rs
+++ examples/uuid/src/main.rs
@@ -9,15 +9,15 @@ extern crate rocket_contrib;
 
 use std::collections::HashMap;
 use uuid::Uuid;
-use rocket_contrib::{UUID, UuidParseError};
+use rocket_contrib::UUID;
 
 #[cfg(test)]
 mod tests;
 
 lazy_static! {
-    // A small people lookup table. In a real application this would be a 
-    // database lookup. Notice that we use the uuid::Uuid type here and not 
-    // rockets UUID type.
+    // A small people lookup table for the sake of this example. In a real 
+    // application this could be a database lookup. Notice that we use the 
+    // uuid::Uuid type here and not the rocket_contrib::UUID type.
     static ref PEOPLE: HashMap<Uuid, &'static str> = {
         let mut m = HashMap::new();
         let lacy_id = Uuid::parse_str("7f205202-7ba1-4c39-b2fc-3e630722bf9f").unwrap();
@@ -32,33 +32,16 @@ lazy_static! {
 
 #[get("/people/<id>")]
 fn people(id: UUID) -> Result<String, String> {
-    // Because UUID implements the Deref trait, we can use Rusts Deref coercion
-    // to convert Rockets UUID type to the uuid::Uuid type.
+    // Because UUID implements the Deref trait, we use Deref coercion to 
+    // convert rocket_contrib::UUID to uuid::Uuid.
     let person = PEOPLE.get(&id)
         .ok_or(format!("Person not found for UUID: {}", id))?;
 
     Ok(format!("We found: {}", person))
-}
-
-
-#[get("/people_opt/<id>")]
-fn people_opt(id: Result<UUID, UuidParseError>) -> Result<String, String> {
-    // If the Uuid can't be parsed, lets go ahead and return the error message 
-    // provided by UuidParseError
-    let id = try!(id.map_err(|err| err.to_string()));
-
-    let person = PEOPLE.get(&id)
-        .ok_or(format!("Person not found for UUID: {}", id))?;
-
-    Ok(format!("We found: {}", person))
-}
-
-fn routes() -> Vec<rocket::Route> {
-    routes![people, people_opt]
 }
 
 fn main() {
     rocket::ignite()
-        .mount("/", routes())
+        .mount("/", routes![people])
         .launch();
 }
--- examples/uuid/src/tests.rs
+++ examples/uuid/src/tests.rs
@@ -4,7 +4,7 @@ use rocket::http::Method::*;
 use rocket::http::Status;
 
 fn test(uri: &str, expected: &str) {
-    let rocket = rocket::ignite().mount("/", super::routes());
+    let rocket = rocket::ignite().mount("/", routes![super::people]);
 
     let mut req = MockRequest::new(Get, uri);
     let mut res = req.dispatch_with(&rocket);
@@ -13,7 +13,7 @@ fn test(uri: &str, expected: &str) {
 }
 
 fn test_404(uri: &str) {
-    let rocket = rocket::ignite().mount("/", super::routes());
+    let rocket = rocket::ignite().mount("/", routes![super::people]);
 
     let mut req = MockRequest::new(Get, uri);
     let res = req.dispatch_with(&rocket);
@@ -30,15 +30,4 @@ fn test_people() {
     test("/people/e18b3a5c-488f-4159-a240-2101e0da19fd", "Person not found for UUID: e18b3a5c-488f-4159-a240-2101e0da19fd");
 
     test_404("/people/invalid_uuid");
-}
-
-#[test]
-fn test_people_opt() {
-    test("/people_opt/7f205202-7ba1-4c39-b2fc-3e630722bf9f", "We found: Lacy");
-    test("/people_opt/4da34121-bc7d-4fc1-aee6-bf8de0795333", "We found: Bob");
-    test("/people_opt/ad962969-4e3d-4de7-ac4a-2d86d6d10839", "We found: George");
-
-    test("/people_opt/e18b3a5c-488f-4159-a240-2101e0da19fd", "Person not found for UUID: e18b3a5c-488f-4159-a240-2101e0da19fd");
-
-    test("/people_opt/invalid_uuid", "Invalid length; expecting 32 or 36 chars, found 12");
 }
