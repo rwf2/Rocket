5623cd4e517284fc73fccc6226100c8d0738fd4b
Crazy-Owl <stalker401@gmail.com>
2017-02-16 07:54:37 +0300

    rearrange arguments of run_test macro

--- examples/json/src/tests.rs
+++ examples/json/src/tests.rs
@@ -5,7 +5,7 @@ use rocket::http::{Status, ContentType};
 use rocket::Response;
 
 macro_rules! run_test {
-    ($req:expr, $test_fn:expr, $rocket:expr) => ({
+    ($rocket: expr, $req:expr, $test_fn:expr) => ({
         let mut req = $req;
         $test_fn(req.dispatch_with($rocket));
     })
@@ -17,36 +17,36 @@ fn bad_get_put() {
 
     // Try to get a message with an ID that doesn't exist.
     let req = MockRequest::new(Get, "/message/99").header(ContentType::JSON);
-    run_test!(req, |mut response: Response| {
+    run_test!(&rocket, req, |mut response: Response| {
         assert_eq!(response.status(), Status::NotFound);
 
         let body = response.body().unwrap().into_string().unwrap();
         assert!(body.contains("error"));
         assert!(body.contains("Resource was not found."));
-    }, &rocket);
+    });
 
     // Try to get a message with an invalid ID.
     let req = MockRequest::new(Get, "/message/hi").header(ContentType::JSON);
-    run_test!(req, |mut response: Response| {
+    run_test!(&rocket, req, |mut response: Response| {
         assert_eq!(response.status(), Status::NotFound);
         let body = response.body().unwrap().into_string().unwrap();
         assert!(body.contains("error"));
-    }, &rocket);
+    });
 
     // Try to put a message without a proper body.
     let req = MockRequest::new(Put, "/message/80").header(ContentType::JSON);
-    run_test!(req, |response: Response| {
+    run_test!(&rocket, req, |response: Response| {
         assert_eq!(response.status(), Status::BadRequest);
-    }, &rocket);
+    });
 
     // Try to put a message for an ID that doesn't exist.
     let req = MockRequest::new(Put, "/message/80")
         .header(ContentType::JSON)
         .body(r#"{ "contents": "Bye bye, world!" }"#);
 
-    run_test!(req, |response: Response| {
+    run_test!(&rocket, req, |response: Response| {
         assert_eq!(response.status(), Status::NotFound);
-    }, &rocket);
+    });
 }
 
 #[test]
@@ -54,42 +54,42 @@ fn post_get_put_get() {
     let rocket = rocket();
     // Check that a message with ID 1 doesn't exist.
     let req = MockRequest::new(Get, "/message/1").header(ContentType::JSON);
-    run_test!(req, |response: Response| {
+    run_test!(&rocket, req, |response: Response| {
         assert_eq!(response.status(), Status::NotFound);
-    }, &rocket);
+    });
 
     // Add a new message with ID 1.
     let req = MockRequest::new(Post, "/message/1")
         .header(ContentType::JSON)
         .body(r#"{ "contents": "Hello, world!" }"#);
 
-    run_test!(req, |response: Response| {
+    run_test!(&rocket, req, |response: Response| {
         assert_eq!(response.status(), Status::Ok);
-    }, &rocket);
+    });
 
     // Check that the message exists with the correct contents.
     let req = MockRequest::new(Get, "/message/1") .header(ContentType::JSON);
-    run_test!(req, |mut response: Response| {
+    run_test!(&rocket, req, |mut response: Response| {
         assert_eq!(response.status(), Status::Ok);
         let body = response.body().unwrap().into_string().unwrap();
         assert!(body.contains("Hello, world!"));
-    }, &rocket);
+    });
 
     // Change the message contents.
     let req = MockRequest::new(Put, "/message/1")
         .header(ContentType::JSON)
         .body(r#"{ "contents": "Bye bye, world!" }"#);
 
-    run_test!(req, |response: Response| {
+    run_test!(&rocket, req, |response: Response| {
         assert_eq!(response.status(), Status::Ok);
-    }, &rocket);
+    });
 
     // Check that the message exists with the updated contents.
     let req = MockRequest::new(Get, "/message/1") .header(ContentType::JSON);
-    run_test!(req, |mut response: Response| {
+    run_test!(&rocket, req, |mut response: Response| {
         assert_eq!(response.status(), Status::Ok);
         let body = response.body().unwrap().into_string().unwrap();
         assert!(!body.contains("Hello, world!"));
         assert!(body.contains("Bye bye, world!"));
-    }, &rocket);
+    });
 }
