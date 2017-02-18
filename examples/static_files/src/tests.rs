23eef9c822f924f5fdeabd0851ffde9e9b3d9d82
mikejiang <mike820324@gmail.com>
2017-02-18 17:00:21 +0800

    Add test for icon and code cleanup.
    - Move the read file content logic into a functin.
      The function will return Vec<u8>
    - Add a function body_to_bytes to tranform body into Vec<u8>.

--- examples/static_files/src/tests.rs
+++ examples/static_files/src/tests.rs
@@ -3,11 +3,27 @@ use std::io::Read;
 use rocket::testing::MockRequest;
 use rocket::http::Method::*;
 use rocket::http::Status;
+use rocket::response::Body;
 
 use super::rocket;
 
+fn body_to_bytes(body: Body<&mut Read>) -> Option<Vec<u8>> {
+    match body {
+        Body::Sized(b, _)  => {
+            let mut data: Vec<u8> = vec![];
+            b.read(&mut data).expect("Read from response failed");
+            return Some(data);
+        },
+        Body::Chunked(b, _) => {
+            let mut data: Vec<u8> = vec![];
+            b.read(&mut data).expect("Read from response failed");
+            return Some(data);
+        }
+    }
+}
+
 fn test_query_file<T> (path: &str, status: Status, content: T)
-    where T: Into<Option<String>>
+    where T: Into<Option<Vec<u8>>>
 {
     let rocket = rocket();
     let mut req = MockRequest::new(Get, &path);
@@ -15,38 +31,37 @@ fn test_query_file<T> (path: &str, status: Status, content: T)
     let mut response = req.dispatch_with(&rocket);
     assert_eq!(response.status(), status);
 
-    let body_str = response.body().and_then(|body| body.into_string());
-    if let Some(expected_str) = content.into() {
-        assert!(body_str.map_or(false, |s| s == expected_str));
+    let body_data = response.body().and_then(|body| body_to_bytes(body));
+    if let Some(expected_data) = content.into() {
+        assert!(body_data.map_or(false, |s| s == expected_data));
     }
+}
+
+fn read_file_content(path: &str) -> Vec<u8> {
+    let mut fp = File::open(&path).expect(&format!("Can not open {}", path));
+
+    let mut file_content:Vec<u8> = vec![];
+    fp.read(&mut file_content).expect(&format!("Reading {} failed.", path));
+
+    file_content
 }
 
 #[test]
 fn test_index_html() {
-    let file_path = "static/index.html";
-
-    let mut fp = File::open(&file_path)
-        .expect(&format!("Can not open {}", file_path));
-
-    let mut expected_content = String::new();
-    fp.read_to_string(&mut expected_content)
-        .expect(&format!("Reading {} failed.", file_path));
-
+    let expected_content = read_file_content("static/index.html");
     test_query_file("/", Status::Ok, expected_content);
 }
 
 #[test]
 fn test_hidden_file() {
-    let file_path = "static/hidden/hi.txt";
-
-    let mut fp = File::open(&file_path)
-        .expect(&format!("Can not open {}", file_path));
-
-    let mut expected_content = String::new();
-    fp.read_to_string(&mut expected_content)
-        .expect(&format!("Reading {} failed.", file_path));
-
+    let expected_content = read_file_content("static/hidden/hi.txt");
     test_query_file("/hidden/hi.txt", Status::Ok, expected_content);
+}
+
+#[test]
+fn test_icon_file() {
+    let expected_content = read_file_content("static/rocket-icon.jpg");
+    test_query_file("/rocket-icon.jpg", Status::Ok, expected_content);
 }
 
 #[test]
