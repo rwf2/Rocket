696440a048191f777574c910b78b47d4bfa36ae0
mikejiang <mike820324@gmail.com>
2017-02-19 14:43:10 +0800

    Code cleanup based on review.
    - Remove redundant code logic in body_to_bytes.
    - Remove unnecessary type annotation.
    - Remove empty lines.
    - Use read_to_end instead of read. (Incomplete read may happened.)
    - Move read_file_content call from test case to test_query_file.

--- examples/static_files/src/tests.rs
+++ examples/static_files/src/tests.rs
@@ -1,5 +1,6 @@
 use std::fs::File;
 use std::io::Read;
+
 use rocket::testing::MockRequest;
 use rocket::http::Method::*;
 use rocket::http::Status;
@@ -9,21 +10,16 @@ use super::rocket;
 
 fn body_to_bytes(body: Body<&mut Read>) -> Option<Vec<u8>> {
     match body {
-        Body::Sized(b, _)  => {
+        Body::Sized(b, _) | Body::Chunked(b, _) => {
             let mut data: Vec<u8> = vec![];
-            b.read(&mut data).expect("Read from response failed");
-            return Some(data);
-        },
-        Body::Chunked(b, _) => {
-            let mut data: Vec<u8> = vec![];
-            b.read(&mut data).expect("Read from response failed");
-            return Some(data);
+            b.read_to_end(&mut data).expect("Read from response failed");
+            Some(data)
         }
     }
 }
 
-fn test_query_file<T> (path: &str, status: Status, content: T)
-    where T: Into<Option<Vec<u8>>>
+fn test_query_file<T> (path: &str, file: T, status: Status)
+    where T: Into<Option<&'static str>>
 {
     let rocket = rocket();
     let mut req = MockRequest::new(Get, &path);
@@ -32,40 +28,37 @@ fn test_query_file<T> (path: &str, status: Status, content: T)
     assert_eq!(response.status(), status);
 
     let body_data = response.body().and_then(|body| body_to_bytes(body));
-    if let Some(expected_data) = content.into() {
+    if let Some(filename) = file.into() {
+        let expected_data = read_file_content(filename);
         assert!(body_data.map_or(false, |s| s == expected_data));
     }
 }
 
 fn read_file_content(path: &str) -> Vec<u8> {
     let mut fp = File::open(&path).expect(&format!("Can not open {}", path));
+    let mut file_content = vec![];
 
-    let mut file_content:Vec<u8> = vec![];
-    fp.read(&mut file_content).expect(&format!("Reading {} failed.", path));
-
+    fp.read_to_end(&mut file_content).expect(&format!("Reading {} failed.", path));
     file_content
 }
 
 #[test]
 fn test_index_html() {
-    let expected_content = read_file_content("static/index.html");
-    test_query_file("/", Status::Ok, expected_content);
+    test_query_file("/", "static/index.html", Status::Ok);
 }
 
 #[test]
 fn test_hidden_file() {
-    let expected_content = read_file_content("static/hidden/hi.txt");
-    test_query_file("/hidden/hi.txt", Status::Ok, expected_content);
+    test_query_file("/hidden/hi.txt", "static/hidden/hi.txt", Status::Ok);
 }
 
 #[test]
 fn test_icon_file() {
-    let expected_content = read_file_content("static/rocket-icon.jpg");
-    test_query_file("/rocket-icon.jpg", Status::Ok, expected_content);
+    test_query_file("/rocket-icon.jpg", "static/rocket-icon.jpg", Status::Ok);
 }
 
 #[test]
 fn test_invalid_path() {
-    test_query_file("/thou_shalt_not_exist", Status::NotFound, None);
-    test_query_file("/thou/shalt/not/exist", Status::NotFound, None);
+    test_query_file("/thou_shalt_not_exist", None, Status::NotFound);
+    test_query_file("/thou/shalt/not/exist", None, Status::NotFound);
 }
