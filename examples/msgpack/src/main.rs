0bd6ae3aa02833ad67de8b1a9f9deecef53986b6
Josh Holmer <joshua.holmer@nextgearcapital.com>
2017-02-24 20:50:29 -0500

    Simplify msgpack example

--- examples/msgpack/src/main.rs
+++ examples/msgpack/src/main.rs
@@ -8,72 +8,24 @@ extern crate rocket_contrib;
 #[cfg(test)] mod tests;
 
 use rocket_contrib::MsgPack;
-use rocket::State;
-use std::collections::HashMap;
-use std::sync::Mutex;
-
-// The type to represent the ID of a message.
-type ID = usize;
-
-// We're going to store all of the messages here. No need for a DB.
-type MessageMap = Mutex<HashMap<ID, String>>;
 
 #[derive(Serialize, Deserialize)]
 struct Message {
-    id: Option<ID>,
+    id: usize,
     contents: String
 }
 
-// TODO: This example can be improved by using `route` with multiple HTTP verbs.
-#[post("/<id>", format = "application/msgpack", data = "<message>")]
-fn new(id: ID, message: MsgPack<Message>, map: State<MessageMap>) -> MsgPack<HashMap<&'static str, &'static str>> {
-    let mut hashmap = map.lock().expect("map lock.");
-    if hashmap.contains_key(&id) {
-        let response = [("status", "error"), ("reason", "ID exists. Try put.")].iter().cloned()
-            .collect::<HashMap<&'static str, &'static str>>();
-        MsgPack(response)
-    } else {
-        hashmap.insert(id, message.0.contents);
-        let response = [("status", "ok")].iter().cloned().collect::<HashMap<&'static str, &'static str>>();
-        MsgPack(response)
-    }
-}
-
-#[put("/<id>", format = "application/msgpack", data = "<message>")]
-fn update(id: ID, message: MsgPack<Message>, map: State<MessageMap>) -> Option<MsgPack<HashMap<&'static str, &'static str>>> {
-    let mut hashmap = map.lock().unwrap();
-    if hashmap.contains_key(&id) {
-        hashmap.insert(id, message.0.contents);
-        let response = [("status", "ok")].iter().cloned().collect::<HashMap<&'static str, &'static str>>();
-        Some(MsgPack(response))
-    } else {
-        None
-    }
-}
-
 #[get("/<id>", format = "application/msgpack")]
-fn get(id: ID, map: State<MessageMap>) -> Option<MsgPack<Message>> {
-    let hashmap = map.lock().unwrap();
-    hashmap.get(&id).map(|contents| {
-        MsgPack(Message {
-            id: Some(id),
-            contents: contents.clone()
-        })
+fn get(id: usize) -> MsgPack<Message> {
+    MsgPack(Message {
+        id: id,
+        contents: "Hello, world!".to_string(),
     })
-}
-
-#[error(404)]
-fn not_found() -> MsgPack<HashMap<&'static str, &'static str>> {
-    let response = [("status", "error"), ("reason", "Resource was not found.")].iter().cloned()
-        .collect::<HashMap<&'static str, &'static str>>();
-    MsgPack(response)
 }
 
 fn rocket() -> rocket::Rocket {
     rocket::ignite()
-        .mount("/message", routes![new, update, get])
-        .catch(errors![not_found])
-        .manage(Mutex::new(HashMap::<ID, String>::new()))
+        .mount("/message", routes![get])
 }
 
 fn main() {
--- examples/msgpack/src/tests.rs
+++ examples/msgpack/src/tests.rs
@@ -5,12 +5,10 @@ use rocket::testing::MockRequest;
 use rocket::http::Method::*;
 use rocket::http::{Status, ContentType};
 use rocket::Response;
-use std::collections::HashMap;
 
 #[derive(Deserialize)]
 struct Message {
-    #[allow(dead_code)]
-    id: Option<usize>,
+    id: usize,
     contents: String
 }
 
@@ -21,107 +19,16 @@ macro_rules! run_test {
     })
 }
 
-fn build_hello_body() -> Vec<u8> {
-    rmp_serde::to_vec(&[("contents", "Hello, world!")]
-        .iter()
-        .cloned()
-        .collect::<HashMap<&'static str, &'static str>>()).unwrap()
-}
-
-fn build_goodbye_body() -> Vec<u8> {
-    rmp_serde::to_vec(&[("contents", "Bye bye, world!")]
-        .iter()
-        .cloned()
-        .collect::<HashMap<&'static str, &'static str>>()).unwrap()
-}
-
 #[test]
-fn bad_get_put() {
+fn msgpack_get() {
     let rocket = rocket();
-
-    // Try to get a message with an ID that doesn't exist.
-    let req = MockRequest::new(Get, "/message/99").header(ContentType::MsgPack);
-    run_test!(&rocket, req, |mut response: Response| {
-        assert_eq!(response.status(), Status::NotFound);
-
-        let body = rmp_serde::from_slice::<HashMap<String, String>>(
-            &response.body().unwrap().into_bytes().unwrap()
-        ).unwrap();
-        assert!(body.values().any(|v| v == "error"));
-        assert!(body.values().any(|v| v == "Resource was not found."));
-    });
-
-    // Try to get a message with an invalid ID.
-    let req = MockRequest::new(Get, "/message/hi").header(ContentType::MsgPack);
-    run_test!(&rocket, req, |mut response: Response| {
-        assert_eq!(response.status(), Status::NotFound);
-        let body = rmp_serde::from_slice::<HashMap<String, String>>(
-            &response.body().unwrap().into_bytes().unwrap()
-        ).unwrap();
-        assert!(body.values().any(|v| v == "error"));
-    });
-
-    // Try to put a message without a proper body.
-    let req = MockRequest::new(Put, "/message/80").header(ContentType::MsgPack);
-    run_test!(&rocket, req, |response: Response| {
-        assert_eq!(response.status(), Status::BadRequest);
-    });
-
-    // Try to put a message for an ID that doesn't exist.
-    let req = MockRequest::new(Put, "/message/80")
-        .header(ContentType::MsgPack)
-        .body(build_goodbye_body());
-
-    run_test!(&rocket, req, |response: Response| {
-        assert_eq!(response.status(), Status::NotFound);
-    });
-}
-
-#[test]
-fn post_get_put_get() {
-    let rocket = rocket();
-    // Check that a message with ID 1 doesn't exist.
-    let req = MockRequest::new(Get, "/message/1").header(ContentType::MsgPack);
-    run_test!(&rocket, req, |response: Response| {
-        assert_eq!(response.status(), Status::NotFound);
-    });
-
-    // Add a new message with ID 1.
-    let req = MockRequest::new(Post, "/message/1")
-        .header(ContentType::MsgPack)
-        .body(build_hello_body());
-
-    run_test!(&rocket, req, |response: Response| {
-        assert_eq!(response.status(), Status::Ok);
-    });
-
-    // Check that the message exists with the correct contents.
     let req = MockRequest::new(Get, "/message/1").header(ContentType::MsgPack);
     run_test!(&rocket, req, |mut response: Response| {
         assert_eq!(response.status(), Status::Ok);
         let body = rmp_serde::from_slice::<Message>(
             &response.body().unwrap().into_bytes().unwrap()
         ).unwrap();
-        assert!(body.contents == "Hello, world!");
-    });
-
-    // Change the message contents.
-    let req = MockRequest::new(Put, "/message/1")
-        .header(ContentType::MsgPack)
-        .body(build_goodbye_body());
-
-    run_test!(&rocket, req, |response: Response| {
-        assert_eq!(response.status(), Status::Ok);
-    });
-
-    // Check that the message exists with the updated contents.
-    let req = MockRequest::new(Get, "/message/1").header(ContentType::MsgPack);
-    run_test!(&rocket, req, |mut response: Response| {
-        assert_eq!(response.status(), Status::Ok);
-        let body = rmp_serde::from_slice::<Message>(
-            &response.body().unwrap().into_bytes().unwrap()
-        ).unwrap();
-        assert!(body.contents != "Hello, world!");
-        assert!(body.contents == "Bye bye, world!");
+        assert_eq!(body.id, 1);
+        assert_eq!(body.contents, "Hello, world!");
     });
 }
