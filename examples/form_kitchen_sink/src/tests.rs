ce07ece51fa84617ee5be59cf6a2e5b7e855bc40
Stefano Chiodino <louderth@gmail.com>
2017-05-25 06:31:56 +0100

    Add test for invalid form input

--- examples/form_kitchen_sink/src/tests.rs
+++ examples/form_kitchen_sink/src/tests.rs
@@ -20,20 +20,26 @@ impl fmt::Display for FormOption {
     }
 }
 
-fn test_post_form(request_form: &FormInput) -> String {
+fn test_post(request_body: &String) -> String {
     let rocket = rocket();
     let mut request = MockRequest::new(Post, "/")
         .header(ContentType::Form)
-        .body(&format!("checkbox={}&number={}&password={}&type={}&textarea={}&select={}",
-            request_form.checkbox,
-            request_form.number,
-            request_form.password,
-            request_form.radio,
-            request_form.text_area,
-            request_form.select));
+        .body(request_body);
 
     let mut response = request.dispatch_with(&rocket);
     response.body_string().unwrap()
+}
+
+fn test_post_form(request_form: &FormInput) -> String {
+    let request_body = format!("checkbox={}&number={}&password={}&type={}&textarea={}&select={}",
+                               request_form.checkbox,
+                               request_form.number,
+                               request_form.password,
+                               request_form.radio,
+                               request_form.text_area,
+                               request_form.select);
+
+    test_post(&request_body)
 }
 
 #[test]
@@ -50,4 +56,13 @@ fn test_good_form() {
     let response = test_post_form(&form_input);
 
     assert_eq!(response, format!("{:?}", form_input));
+}
+
+#[test]
+fn test_invalid_form() {
+    let input = "wrong form input".to_string();
+
+    let response = test_post(&input);
+
+    assert_eq!(response, format!("Invalid form input: {}", input));
 }
