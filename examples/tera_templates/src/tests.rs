6ebf5670c56ca3d34661002074a0d039217a3652
lerina (J-Y) RAZAFY <icode.lerina@gmail.com>
2017-06-02 17:37:27 -0400

    using the same test as handlebars_templates

--- examples/tera_templates/src/tests.rs
+++ examples/tera_templates/src/tests.rs
@@ -1,17 +1,16 @@
-use rocket;
+use super::rocket;
 use rocket::testing::MockRequest;
 use rocket::http::Method::*;
 use rocket::http::Status;
 use rocket::Response;
 use rocket_contrib::Template;
 
-macro_rules! run_test {
-    ($req:expr, $test_fn:expr) => ({
-        let rocket = rocket::ignite()
-            .mount("/", routes![super::index, super::get])
-            .catch(errors![super::not_found]);
+const TEMPLATE_ROOT: &'static str = "templates/";
 
-        let mut req = $req;
+macro_rules! dispatch {
+    ($method:expr, $path:expr, $test_fn:expr) => ({
+        let rocket = rocket();
+        let mut req = MockRequest::new($method, $path);
         $test_fn(req.dispatch_with(&rocket));
     })
 }
@@ -20,26 +19,24 @@ macro_rules! run_test {
 fn test_root() {
     // Check that the redirect works.
     for method in &[Get, Head] {
-        let req = MockRequest::new(*method, "/");
-        run_test!(req, |mut response: Response| {
+        dispatch!(*method, "/", |mut response: Response| {
             assert_eq!(response.status(), Status::SeeOther);
             assert!(response.body().is_none());
 
-            let location_headers: Vec<_> = response.headers().get("Location").collect();
-            assert_eq!(location_headers, vec!["/hello/Unknown"]);
+            let location: Vec<_> = response.headers().get("Location").collect();
+            assert_eq!(location, vec!["/hello/Unknown"]);
         });
     }
 
     // Check that other request methods are not accepted (and instead caught).
     for method in &[Post, Put, Delete, Options, Trace, Connect, Patch] {
-        let req = MockRequest::new(*method, "/");
-        run_test!(req, |mut response: Response| {
+        dispatch!(*method, "/", |mut response: Response| {
             let mut map = ::std::collections::HashMap::new();
             map.insert("path", "/");
-            let expected_body = Template::render("error/404", &map).to_string();
+            let expected = Template::show(TEMPLATE_ROOT, "error/404", &map).unwrap();
 
             assert_eq!(response.status(), Status::NotFound);
-            assert_eq!(response.body_string(), Some(expected_body));
+            assert_eq!(response.body_string(), Some(expected));
         });
     }
 }
@@ -47,16 +44,14 @@ fn test_root() {
 #[test]
 fn test_name() {
     // Check that the /hello/<name> route works.
-    let req = MockRequest::new(Get, "/hello/Jack");
-    run_test!(req, |mut response: Response| {
-        assert_eq!(response.status(), Status::Ok);
-
+    dispatch!(Get, "/hello/Jack", |mut response: Response| {
         let context = super::TemplateContext {
             name: "Jack".to_string(),
             items: vec!["One", "Two", "Three"].iter().map(|s| s.to_string()).collect()
         };
 
-        let expected = Template::render("index", &context).to_string();
+        let expected = Template::show(TEMPLATE_ROOT, "index", &context).unwrap();
+        assert_eq!(response.status(), Status::Ok);
         assert_eq!(response.body_string(), Some(expected));
     });
 }
@@ -64,13 +59,12 @@ fn test_name() {
 #[test]
 fn test_404() {
     // Check that the error catcher works.
-    let req = MockRequest::new(Get, "/hello/");
-    run_test!(req, |mut response: Response| {
-        assert_eq!(response.status(), Status::NotFound);
-
+    dispatch!(Get, "/hello/", |mut response: Response| {
         let mut map = ::std::collections::HashMap::new();
         map.insert("path", "/hello/");
-        let expected = Template::render("error/404", &map).to_string();
+
+        let expected = Template::show(TEMPLATE_ROOT, "error/404", &map).unwrap();
+        assert_eq!(response.status(), Status::NotFound);
         assert_eq!(response.body_string(), Some(expected));
     });
 }
