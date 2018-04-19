fa40c74d32b163630f5d308b7f43f964da107783
Martin1887 <marcos.martin.pozo.delgado@gmail.com>
2018-04-19 18:56:08 +0200

    [CompressionFairing] Some refactoring of the code and streaming the compression instead of read and write the whole response at once

--- contrib/src/compression.rs
+++ contrib/src/compression.rs
@@ -21,12 +21,15 @@
 //! For gzip compression, flate2 crate is used.
 
 use std::io::Cursor;
+use std::io::Read;
+use std::io::Seek;
+use std::io::SeekFrom;
 #[cfg(feature = "gzip_compression")]
 use std::io::Write;
 
-use rocket::{Request, Response};
-use rocket::http::Header;
 use rocket::fairing::{Fairing, Info, Kind};
+use rocket::http::Header;
+use rocket::{Request, Response};
 
 #[cfg(feature = "brotli_compression")]
 use brotli;
@@ -52,6 +55,25 @@ impl Compression {
     pub fn fairing() -> Compression {
         Compression::default()
     }
+
+    fn accepts_encoding(request: &Request, encodings: &[&str]) -> bool {
+        request
+            .headers()
+            .get("Accept-Encoding")
+            .flat_map(|e| e.split(","))
+            .any(|e| encodings.contains(&e.trim()))
+    }
+
+    fn set_body_and_header<'h, B>(response: &mut Response<'h>, mut body: B, header: &'h str)
+    where
+        B: Read + Seek + 'h,
+    {
+        if body.seek(SeekFrom::Start(0)).is_ok() {
+            response.remove_header("Content-Encoding");
+            response.adjoin_header(Header::new("Content-Encoding", header));
+            response.set_streamed_body(body);
+        }
+    }
 }
 
 impl Fairing for Compression {
@@ -74,18 +96,14 @@ impl Fairing for Compression {
         // The compression is done if the request supports brotli or gzip and
         // the corresponding feature is enabled
         if cfg!(feature = "brotli_compression")
-            && request
-                .headers()
-                .get("Accept-Encoding")
-                .flat_map(|e| e.trim().split(","))
-                .any(|e| e.trim() == "br" || e.trim() == "brotli")
+            && Compression::accepts_encoding(request, &["br", "brotli"])
         {
-            #[cfg(feature = "brotli_compression")]
+            let mut success = false;
+            let mut plain_buffer: Vec<u8> = Vec::new();
+            let mut compressed = Cursor::new(Vec::<u8>::new());
             {
-                let body = response.body_bytes();
-                if let Some(body) = body {
-                    let mut plain = Cursor::new(body);
-                    let mut compressed = Cursor::new(Vec::<u8>::new());
+                let plain = response.body();
+                if let Some(plain) = plain {
                     let mut params = brotli::enc::BrotliEncoderInitParams();
                     params.quality = 2;
                     if let Some(ref content_type) = content_type {
@@ -95,34 +113,38 @@ impl Fairing for Compression {
                             params.mode = BrotliEncoderMode::BROTLI_MODE_FONT;
                         }
                     }
-                    if brotli::BrotliCompress(&mut plain, &mut compressed, &params).is_ok() {
-                        response.remove_header("Content-Encoding");
-                        response.adjoin_header(Header::new("Content-Encoding", "br"));
-                        response.set_sized_body(compressed);
+                    success = plain.into_inner().read_to_end(&mut plain_buffer).is_ok();
+                    if success {
+                        success = brotli::BrotliCompress(
+                            &mut Cursor::new(plain_buffer),
+                            &mut compressed,
+                            &params,
+                        ).is_ok();
                     }
                 }
             }
+            if success {
+                Compression::set_body_and_header(response, compressed, "br");
+            }
         } else if cfg!(feature = "gzip_compression")
-            && request
-                .headers()
-                .get("Accept-Encoding")
-                .flat_map(|e| e.trim().split(","))
-                .any(|e| e.trim() == "gzip")
+            && Compression::accepts_encoding(request, &["gzip"])
         {
-            #[cfg(feature = "gzip_compression")]
+            let mut success = false;
+            let mut plain_buffer: Vec<u8> = Vec::new();
+            let mut compressed = Cursor::new(Vec::<u8>::new());
             {
-                let body = response.body_bytes();
-                if let Some(body) = body {
-                    let mut compressed = Cursor::new(Vec::<u8>::new());
-                    if GzEncoder::new(&mut compressed, flate2::Compression::default())
-                        .write_all(&body)
-                        .is_ok()
-                    {
-                        response.remove_header("Content-Encoding");
-                        response.adjoin_header(Header::new("Content-Encoding", "gzip"));
-                        response.set_sized_body(compressed);
+                let plain = response.body();
+                if let Some(plain) = plain {
+                    success = plain.into_inner().read_to_end(&mut plain_buffer).is_ok();
+                    if success {
+                        success = GzEncoder::new(&mut compressed, flate2::Compression::default())
+                            .write_all(&plain_buffer)
+                            .is_ok();
                     }
                 }
+            }
+            if success {
+                Compression::set_body_and_header(response, compressed, "gzip");
             }
         }
     }
