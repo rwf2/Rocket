9a704fdc4074e917d6bef81005432fe2ffccf723
Kmoneal <kmacjoneal@gmail.com>
2018-06-26 17:08:39 -0700

    Add client certificate verification.
    * Lookup domain name associated with client's IP Address.
    * Verify that the domain name match the certificate common name.

--- core/http/src/tls.rs
+++ core/http/src/tls.rs
@@ -1,5 +1,11 @@
 extern crate rustls;
 extern crate hyper_sync_rustls;
+extern crate dns_lookup;
+extern crate untrusted;
+extern crate webpki;
 
 pub use self::hyper_sync_rustls::{util, WrappedStream, ServerSession, TlsServer};
 pub use self::rustls::{Certificate, PrivateKey, RootCertStore};
+pub use self::dns_lookup::lookup_addr;
+pub use self::untrusted::Input;
+pub use self::webpki::{EndEntityCert, DNSNameRef};
