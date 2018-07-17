03f2c0da0ab3918ec3280f8893b3ee014001b92b
Kmoneal <kmacjoneal@gmail.com>
2018-07-17 13:28:21 -0700

    Make MutualTlsUser more robust.
    * Add more comments explaining sections of code.
    * Add documentation and examples.
    * Remove public key and signature from MutualTlsUser.
    * Improve error handling to not panic when generating a new
      MutualTlsUser.
    * Replace unwraps with exceptions to specify what failed.

--- core/http/src/tls.rs
+++ core/http/src/tls.rs
@@ -6,83 +6,140 @@ extern crate webpki;
 extern crate openssl;
 
 pub use self::hyper_sync_rustls::{util, WrappedStream, ServerSession, TlsServer};
-pub use self::rustls::{Certificate, PrivateKey, RootCertStore};
+pub use self::rustls::{Certificate, PrivateKey, RootCertStore, internal::pemfile};
 pub use self::dns_lookup::lookup_addr;
 pub use self::untrusted::Input;
 pub use self::webpki::{EndEntityCert, DNSNameRef};
 
 use self::openssl::x509::X509;
 
-//#[derive(Debug)]
+/// Client MTLS certificate information.
+///
+/// The `MutualTlsUser` type specifies MTLS being required for the route and retrieves client
+/// certificate information.
+///
+/// #Usage
+///
+/// A `MutualTlsUser` can be retrieved via its `FromRequest` implementation as a request guard.
+/// Information of the certificate with a matching common name as a reverse DNS lookup of the
+/// client IP address from the accepted certificate chain can be retrieved via the
+/// `get_common_names`, `get_not_before`, `get_not_after`, `get_pem_public_key`,
+/// `get_der_public_key`, and `get_signature` methods.
+///
+/// ##Examples
+///
+/// The following short snippet shows `MutualTlsUser` being used as a request guard in a handler to
+/// verify the client's certificate and print the common names of the client.
+///
+/// ```rust
+/// # #![feature(plugin, decl_macro)]
+/// # #![plugin(rocket_codegen)]
+/// # extern crate rocket;
+/// use rocket::http::tls::MutualTlsUser;
+///
+/// #[get("/message")]
+/// fn message(mtls:MutualTlsUser) {
+///     let common_names = mtls.get_common_names();
+///     for name in common_names {
+///         println!("{}", name);
+///     }
+/// }
+///
+/// # fn main() { }
+/// ```
+///
+#[derive(Debug)]
 pub struct MutualTlsUser {
     common_names: Vec<String>,
     not_before: String,
     not_after: String,
-    pem_public_key: Vec<u8>,
-    der_public_key: Vec<u8>,
-    signature: Vec<u8>,
 }
 
 impl MutualTlsUser {
-    pub fn new(peer_cert: Certificate) -> MutualTlsUser {
-        let x509 = X509::from_der(peer_cert.as_ref()).expect("certificate could not be parsed");
+    pub fn new(peer_cert: Certificate) -> Option<MutualTlsUser> {
+        // Generate an x509 using the certificate provided
+        let x509 = X509::from_der(peer_cert.as_ref());
+        if x509.is_err() {
+            return None;
+        }
+        let x509 = x509.expect("Failed to generate X509.");
 
-        let alt_names = x509.subject_alt_names().expect("subject alt names could not be retrieved");
+        // Retrieve alt names and store them into a Vec<String>
+        let alt_names = x509.subject_alt_names();
+        if alt_names.is_none() {
+            return None;
+        }
+        let alt_names = alt_names.expect("Alt names for certificate do not exist.");
         let mut common_names = Vec::new();
         for name in alt_names {
-            let name = name.dnsname().expect("common name could not be converted to dns name").clone();
+            let name = name.dnsname();
+            if name.is_none() {
+                return None;
+            }
+            let name = name.expect("Name does not exist.");
             common_names.push(format!("{}", name));
         }
 
+        // Retrieve certificate start time
         let not_before = x509.not_before().clone();
         let not_before = format!("{}", not_before);
 
+        // Retrieve certificate end time
         let not_after = x509.not_after().clone();
         let not_after = format!("{}", not_after);
 
-        let pem_public_key = x509.public_key().unwrap().public_key_to_pem().unwrap();
-
-        let der_public_key = x509.public_key().unwrap().public_key_to_der().unwrap();
-
-        let signature = x509.signature().as_slice().to_vec();
-
-        MutualTlsUser {
+        Some(MutualTlsUser {
             common_names,
             not_before,
             not_after,
-            pem_public_key,
-            der_public_key,
-            signature,
-        }
+        })
     }
 
-    /// Get the client's common names
+    /// Return a clone of the client's common names.
+    ///
+    /// # Example
+    ///
+    /// ```rust
+    /// # extern crate rocket;
+    /// use rocket::http::tls::MutualTlsUser;
+    ///
+    /// fn handler(mtls: MutualTlsUser) {
+    ///     let cert_common_names = mtls.get_common_names();
+    /// }
+    /// ```
     pub fn get_common_names(&self) -> Vec<String> {
         self.common_names.clone()
     }
 
-    /// Get the client's validity period not before
+    /// Return a clone of the client's certificate's validity period start time.
+    ///
+    /// # Example
+    ///
+    /// ```rust
+    /// # extern crate rocket;
+    /// use rocket::http::tls::MutualTlsUser;
+    ///
+    /// fn handler(mtls: MutualTlsUser) {
+    ///     let cert_start_time = mtls.get_validity_period_not_before();
+    /// }
+    /// ```
     pub fn get_validity_period_not_before(&self) -> String {
         self.not_before.clone()
     }
 
-    /// Get the client's validity period not after
+    /// Return a clone of the client's certificate's validity period end time.
+    ///
+    /// # Example
+    ///
+    /// ```rust
+    /// # extern crate rocket;
+    /// use rocket::http::tls::MutualTlsUser;
+    ///
+    /// fn handler(mtls: MutualTlsUser) {
+    ///     let cert_end_time = mtls.get_validity_period_not_after();
+    /// }
+    /// ```
     pub fn get_validity_period_not_after(&self) -> String {
         self.not_after.clone()
-    }
-
-    /// Get the client's public key in pem format
-    pub fn get_pem_public_key(&self) -> Vec<u8> {
-        self.pem_public_key.clone()
-    }
-
-    /// Get the client's public key in der format
-    pub fn get_der_public_key(&self) -> Vec<u8> {
-        self.der_public_key.clone()
-    }
-
-    /// Get the client's signature
-    pub fn get_signature(&self) -> Vec<u8> {
-        self.signature.clone()
     }
 }
