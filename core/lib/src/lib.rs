02a98feea5e370edb806e0268c034869cc816b4a
Jacob Pratt <the.z.cuber@gmail.com>
2019-08-20 19:53:00 -0400

    Remove stabilized 'async_await' feature gate.

--- core/lib/src/lib.rs
+++ core/lib/src/lib.rs
@@ -3,7 +3,6 @@
 #![feature(proc_macro_hygiene)]
 #![feature(crate_visibility_modifier)]
 #![feature(label_break_value)]
-#![feature(async_await)]
 
 #![recursion_limit="256"]
 
@@ -52,7 +51,7 @@
 //! Then, add the following to the top of your `main.rs` file:
 //!
 //! ```rust
-//! #![feature(proc_macro_hygiene, async_await)]
+//! #![feature(proc_macro_hygiene)]
 //!
 //! #[macro_use] extern crate rocket;
 //! # #[get("/")] fn hello() { }
@@ -63,7 +62,7 @@
 //! write Rocket applications. Here's a simple example to get you started:
 //!
 //! ```rust
-//! #![feature(proc_macro_hygiene, async_await)]
+//! #![feature(proc_macro_hygiene)]
 //!
 //! #[macro_use] extern crate rocket;
 //!
