190301914968d3c3b96d0d88cf2b8d7b8c9581e2
Michael Fletcher <m.fletcher@theplanet.ca>
2019-09-20 19:48:48 -0600

    Simplified method integration tests.

--- contrib/lib/tests/cors.rs
+++ contrib/lib/tests/cors.rs
@@ -11,8 +11,6 @@
 // note, I think all of these can be integration tests
 #[cfg(feature = "cors")]
 mod cors_tests {
-    use rocket::http::Method;
-    use rocket::Route;
     use rocket_contrib::cors::*;
     use rocket::local::*;
     use rocket::http::Status;
@@ -40,32 +38,32 @@ mod cors_tests {
     //   `Access-Control-Allow-Headers` 
     //   `Access-Control-Max-Age` 
     //   `Access-Control-Expose-Headers` 
-    
 
     #[test]
     pub fn test_one_method() {
         let rocket = rocket::ignite()
-            .mount("/", routes![sample_get_route, sample_delete_route])
+            .mount("/", routes![sample_delete_route])
             .attach(CorsFairingBuilder::new()
                 .build());
 
-        let routes : Vec<&Route> = rocket.routes()
-            .filter(|x| x.method == Method::Options)
-            .filter(|x| x.uri.path() == "/test")
-            .collect();
-        assert_eq!(1, routes.len());
-
         let client = Client::new(rocket).expect("valid rocket instance");
-
-        // Dispatch a request to 'GET /' and validate the response.
         let response = client.options("/test").dispatch();
         assert_eq!(response.status(), Status::Ok);
-        assert_eq!(response.headers().get_one("Access-Control-Allow-Methods"), Some("DELETE, GET".into()));
+        assert_eq!(response.headers().get_one("Access-Control-Allow-Methods"), Some("DELETE".into()));
     }
 
     // Test to ensure the method names are collected when multiple
     #[test]
     pub fn test_many_method() {
+        let rocket = rocket::ignite()
+            .mount("/", routes![sample_get_route, sample_delete_route])
+            .attach(CorsFairingBuilder::new()
+                .build());
 
+        let client = Client::new(rocket).expect("valid rocket instance");
+
+        let response = client.options("/test").dispatch();
+        assert_eq!(response.status(), Status::Ok);
+        assert_eq!(response.headers().get_one("Access-Control-Allow-Methods"), Some("DELETE, GET".into()));
     }
 }
