46ece3b27085e25c5584e02e9f2b73c4bcd83a47
Jeb Rosen <jeb@jebrosen.com>
2020-03-03 17:47:04 -0800

    Isolate differences between async_test and main attribute implementations to a Kind type.

--- core/codegen/src/attribute/async_entry.rs
+++ core/codegen/src/attribute/async_entry.rs
@@ -1,7 +1,40 @@
 use proc_macro::{TokenStream, Span};
 use devise::{syn, Result};
 
+use crate::proc_macro2::TokenStream as TokenStream2;
 use crate::syn_ext::syn_to_diag;
+
+#[derive(Copy, Clone)]
+enum Kind {
+    Main,
+    Test,
+}
+
+impl Kind {
+    // The name of the attribute, used for error messages
+    fn attr_name(&self) -> &'static str {
+        match self {
+            Kind::Main => "main",
+            Kind::Test => "async_test",
+        }
+    }
+
+    // Attributes to decorate the generated function with
+    fn attrs(&self) -> Option<TokenStream2> {
+        match self {
+            Kind::Main => None,
+            Kind::Test => Some(quote!{ #[test] }),
+        }
+    }
+
+    // The path to the function to call
+    fn fn_path(&self) -> TokenStream2 {
+        match self {
+            Kind::Main => quote! { rocket :: async_main },
+            Kind::Test => quote! { rocket :: async_test },
+        }
+    }
+}
 
 fn parse_input(input: TokenStream, attr_name: &str) -> Result<syn::ItemFn> {
     let function: syn::ItemFn = syn::parse(input).map_err(syn_to_diag)
@@ -18,8 +51,8 @@ fn parse_input(input: TokenStream, attr_name: &str) -> Result<syn::ItemFn> {
     Ok(function)
 }
 
-pub fn _async_entry(_args: TokenStream, input: TokenStream, attr_name: &str, is_test: bool) -> Result<TokenStream> {
-    let function = parse_input(input, attr_name)?;
+fn _async_entry(_args: TokenStream, input: TokenStream, kind: Kind) -> Result<TokenStream> {
+    let function = parse_input(input, kind.attr_name())?;
 
     let attrs = &function.attrs;
     let vis = &function.vis;
@@ -27,17 +60,14 @@ pub fn _async_entry(_args: TokenStream, input: TokenStream, attr_name: &str, is_
     let output = &function.sig.output;
     let body = &function.block;
 
-    let (test_attr, fn_name) = if is_test {
-        (Some(quote! { #[test] } ), quote! { async_test })
-    } else {
-        (None, quote! { async_main })
-    };
+    let test_attr = kind.attrs();
+    let fn_path = kind.fn_path();
 
     Ok(quote! {
         #test_attr
         #(#attrs)*
         #vis fn #name() #output {
-            rocket :: #fn_name (async move {
+            #fn_path (async move {
                 #body
             })
         }
@@ -45,9 +75,9 @@ pub fn _async_entry(_args: TokenStream, input: TokenStream, attr_name: &str, is_
 }
 
 pub fn async_test_attribute(args: TokenStream, input: TokenStream) -> TokenStream {
-    _async_entry(args, input, "async_test", true).unwrap_or_else(|d| { d.emit(); TokenStream::new() })
+    _async_entry(args, input, Kind::Test).unwrap_or_else(|d| { d.emit(); TokenStream::new() })
 }
 
 pub fn main_attribute(args: TokenStream, input: TokenStream) -> TokenStream {
-    _async_entry(args, input, "main", false).unwrap_or_else(|d| { d.emit(); TokenStream::new() })
+    _async_entry(args, input, Kind::Main).unwrap_or_else(|d| { d.emit(); TokenStream::new() })
 }
