d93b4b199093f00aef36e79ffef9443023e643c1
Lorran Rosa <rosalorran98@gmail.com>
2020-04-15 08:22:59 -0300

    Fixing form tests

--- core/lib/tests/form_method-issue-45.rs
+++ core/lib/tests/form_method-issue-45.rs
@@ -39,6 +39,6 @@ mod tests {
             .body("_method=patch&form_data=Form+data")
             .dispatch();
 
-        assert_eq!(response.status(), Status::NotFound);
+        assert_eq!(response.status(), Status::MethodNotAllowed);
     }
 }
