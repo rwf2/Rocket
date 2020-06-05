f88197349808cbe8edcb8e3d6dbf976f9988a49a
Jeb Rosen <jeb@jebrosen.com>
2020-06-05 13:57:02 -0700

    Update UI tests for latest nightly.

--- contrib/codegen/tests/ui-fail/database-types.rs
+++ contrib/codegen/tests/ui-fail/database-types.rs
@@ -4,23 +4,11 @@ extern crate rocket;
 struct Unknown;
 
 #[database("foo")]
-//~^ ERROR Unknown: rocket_contrib::databases::Poolable
-//~^^ ERROR is private
-//~^^^ ERROR no method named `get`
-//~^^^^ ERROR no function or associated item
-//~^^^^^ ERROR Unknown: rocket_contrib::databases::Poolable
 struct A(Unknown);
 //~^ ERROR Unknown: rocket_contrib::databases::Poolable
-//~^^ ERROR Unknown: rocket_contrib::databases::Poolable
 
 #[database("foo")]
-//~^ ERROR Vec<i32>: rocket_contrib::databases::Poolable
-//~^^ ERROR is private
-//~^^^ ERROR no method named `get`
-//~^^^^ ERROR no function or associated item
-//~^^^^^ ERROR Vec<i32>: rocket_contrib::databases::Poolable
 struct B(Vec<i32>);
 //~^ ERROR Vec<i32>: rocket_contrib::databases::Poolable
-//~^^ ERROR Vec<i32>: rocket_contrib::databases::Poolable
 
 fn main() {  }
