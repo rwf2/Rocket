91221053799cbe4b82747484126e3183d7069d02
Michael Howell <michael@notriddle.com>
2020-07-03 22:00:29 +0000

    Add test case for graceful shutdown

---
+++ core/lib/tests/graceful-shutdown.rs
@@ -0,0 +1,44 @@
+#![feature(proc_macro_hygiene)]
+
+#[macro_use] extern crate rocket;
+
+use rocket::shutdown::ShutdownHandle;
+use rocket::response::Response;
+use tokio::io::AsyncRead;
+
+use std::pin::Pin;
+use std::task::{Poll, Context};
+
+struct AsyncReader;
+
+impl AsyncRead for AsyncReader {
+    fn poll_read(
+        self: Pin<&mut Self>,
+        _cx: &mut Context,
+        _buf: &mut [u8]
+    ) -> Poll<tokio::io::Result<usize>> {
+        Poll::Pending
+    }
+}
+
+#[get("/test")]
+fn test(shutdown: ShutdownHandle) -> Response<'static> {
+    shutdown.shutdown();
+    Response::build()
+    .chunked_body(AsyncReader, 512)
+    .finalize()
+}
+
+mod tests {
+    use super::*;
+    use rocket::local::Client;
+
+    #[rocket::async_test]
+    async fn graceful_shutdown_works() {
+        let rocket = rocket::ignite()
+            .mount("/", routes![test]);
+        let client = Client::new(rocket).await.unwrap();
+
+        let _ = client.get("/test").dispatch().await;
+    }
+}
