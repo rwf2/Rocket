e0e08ffc8d8f61f2697682a7c1e76d4642441d68
lewis <me@lewis.to>
2020-09-08 20:42:55 +0100

    Oops - actually give it an encoded string

--- core/lib/tests/form_value_from_encoded_str-issue-1425.rs
+++ core/lib/tests/form_value_from_encoded_str-issue-1425.rs
@@ -22,17 +22,17 @@ mod tests {
     #[test]
     fn test_from_form_value_encoded() {
         check_from_form_value_encoded(
-            "127.0.0.1:80",
+            "127.0.0.1%3A80",
             SocketAddrV4::new(Ipv4Addr::new(127, 0, 0, 1), 80),
         );
 
         check_from_form_value_encoded(
-            "2001:0db8:85a3:0000:0000:8a2e:0370:7334",
+            "2001%3A0db8%3A85a3%3A0000%3A0000%3A8a2e%3A0370%3A7334",
             Ipv6Addr::new(0x2001, 0x0db8, 0x85a3, 0, 0, 0x8a2e, 0x0370, 0x7334),
         );
 
         check_from_form_value_encoded(
-            "[2001:db8::1]:8080",
+            "%5B2001%3Adb8%3A%3A1%5D%3A8080",
             SocketAddrV6::new(Ipv6Addr::new(0x2001, 0xdb8, 0, 0, 0, 0, 0, 1), 8080, 0, 0),
         );
     }
