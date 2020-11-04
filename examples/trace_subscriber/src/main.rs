6de118051e57e0bd87cf0f6f61e4a98739081add
Eliza Weisman <eliza@buoyant.io>
2020-11-04 10:29:20 -0800

    make examples a little fancier
    Signed-off-by: Eliza Weisman <eliza@buoyant.io>

--- examples/trace_subscriber/src/main.rs
+++ examples/trace_subscriber/src/main.rs
@@ -2,14 +2,26 @@ use rocket::trace;
 
 #[rocket::get("/hello/<name>/<age>")]
 fn hello(name: String, age: u8) -> String {
-    trace::info!(?name, age, "saying hello to");
-    format!("Hello, {} year old named {}!", age, name)
+    trace::info!("saying hello...");
+    greet(name, Some(age))
 }
 
 #[rocket::get("/hello/<name>")]
 fn hi(name: String) -> String {
     trace::info!(?name, "saying hi to");
-    name
+    greet(name, None)
+}
+
+#[trace::instrument(level = "info")]
+fn greet(name: String, age: Option<u8>) -> String {
+    trace::debug!(?age);
+    if let Some(age) = age {
+        trace::debug!("found an age, saying hello");
+        format!("Hello, {} year old named {}!", age, name)
+    } else {
+        trace::debug!("no age, just saying hi...");
+        name
+    }
 }
 
 #[rocket::launch]
@@ -19,9 +31,14 @@ fn rocket() -> rocket::Rocket {
     let figment = rocket::Config::figment();
     let config = rocket::Config::from(&figment);
 
+    // Use Rocket's default log filter...
+    let filter = rocket::trace::filter_layer(config.log_level)
+        // ...but enable the debug level for the app crate.
+        .add_directive("trace_subscriber=debug".parse().unwrap());
+
     rocket::trace::registry()
         .with(tracing_subscriber::fmt::layer())
-        .with(rocket::trace::filter_layer(config.log_level))
+        .with(filter)
         .init();
 
     rocket::custom(figment).mount("/", rocket::routes![hello, hi])
