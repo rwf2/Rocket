cd51e4e41c088b6865d73f4c3db1af54e0c6860c
Matt Fellenz <matt@felle.nz>
2022-03-07 08:23:26 -0800

    Fix config test to use new API

--- examples/config/src/tests.rs
+++ examples/config/src/tests.rs
@@ -1,4 +1,5 @@
-use rocket::config::{Config, LogLevel};
+use rocket::config::{Config, LogLevel, BindableAddr};
+use std::net::{Ipv4Addr, SocketAddr};
 
 async fn test_config(profile: &str) {
     let provider = Config::figment().select(profile);
@@ -6,15 +7,13 @@ async fn test_config(profile: &str) {
     let config = rocket.config();
     match &*profile {
         "debug" => {
-            assert_eq!(config.address, std::net::Ipv4Addr::LOCALHOST);
-            assert_eq!(config.port, 8000);
+            assert_eq!(config.address, BindableAddr::Tcp(SocketAddr::new(std::net::Ipv4Addr::LOCALHOST.into(), 8000));
             assert_eq!(config.workers, 1);
             assert_eq!(config.keep_alive, 0);
             assert_eq!(config.log_level, LogLevel::Normal);
         }
         "release" => {
-            assert_eq!(config.address, std::net::Ipv4Addr::LOCALHOST);
-            assert_eq!(config.port, 8000);
+            assert_eq!(config.address, BindableAddr::Tcp(SocketAddr::new(std::net::Ipv4Addr::LOCALHOST.into(), 8000));
             assert_eq!(config.workers, 12);
             assert_eq!(config.keep_alive, 5);
             assert_eq!(config.log_level, LogLevel::Critical);
