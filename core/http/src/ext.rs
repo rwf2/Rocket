531188a9c7306d4640ec5b1f3b320aaf31372968
Youngsuk Kim <joseph942010@gmail.com>
2022-03-19 23:43:26 -0400

    Replace deprecated 'std::array::IntoIter::new()'
    Non-Functional-change.
    Helps reduce noise from "deprecated" warnings :)

--- core/http/src/ext.rs
+++ core/http/src/ext.rs
@@ -1,8 +1,5 @@
 //! Extension traits implemented by several HTTP types.
 
-// Temporarily allow `IntoIter::into_iter()` before Rust 2021 transition.
-#![allow(deprecated)]
-
 use smallvec::{Array, SmallVec};
 use state::Storage;
 
@@ -66,14 +63,14 @@ impl<T: Clone> IntoCollection<T> for &[T] {
 impl<T, const N: usize> IntoCollection<T> for [T; N] {
     #[inline(always)]
     fn into_collection<A: Array<Item=T>>(self) -> SmallVec<A> {
-        std::array::IntoIter::new(self).collect()
+        Self::into_iter(self).collect()
     }
 
     #[inline]
     fn mapped<U, F, A: Array<Item=U>>(self, f: F) -> SmallVec<A>
         where F: FnMut(T) -> U
     {
-        std::array::IntoIter::new(self).map(f).collect()
+        Self::into_iter(self).map(f).collect()
     }
 }
 
