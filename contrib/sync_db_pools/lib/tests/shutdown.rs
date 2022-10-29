ae744f5502cd152e986c8c721e3fc7da3947c428
Nya <7364785+liquidnya@users.noreply.github.com>
2022-10-29 12:12:07 +0200

    Use async_test instead of async_run

--- contrib/sync_db_pools/lib/tests/shutdown.rs
+++ contrib/sync_db_pools/lib/tests/shutdown.rs
@@ -1,7 +1,7 @@
 #[cfg(all(feature = "diesel_sqlite_pool"))]
 #[cfg(test)]
 mod sqlite_shutdown_test {
-    use rocket::{async_run, Build, Rocket};
+    use rocket::{async_test, Build, Rocket};
     use rocket_sync_db_pools::database;
 
     #[database("test")]
@@ -19,17 +19,14 @@ mod sqlite_shutdown_test {
 
     #[test]
     fn test_shutdown() {
-        let _ = async_run(
+        let _rocket = async_test(
             async {
                 let rocket = rocket().await.ignite().await.expect("unable to ignite");
                 // request shutdown
                 rocket.shutdown().notify();
                 rocket.launch().await.expect("unable to launch")
-            },
-            1,
-            32,
-            true, // if `force_end` is set to true, then the runtime is stopped before the Pool is dropped
-            "rocket-worker-shutdown-test-thread",
+            }
         );
+        // _rocket is dropped here after the runtime is dropped
     }
 }
