d98c787ae0aaad73fcabafd080528364a541714c
Dan Dumont <dan.dumont@hcl.com>
2024-03-11 11:48:40 -0400

    return NotModified if resource has not been modified.

--- core/lib/src/fs/server_file.rs
+++ core/lib/src/fs/server_file.rs
@@ -1,5 +1,7 @@
 use std::io;
 
+use http::header::IF_MODIFIED_SINCE;
+use rocket_http::Status;
 use time::OffsetDateTime;
 
 use crate::fs::MaybeCompressedFile;
@@ -12,7 +14,7 @@ use crate::Request;
 /// [`FileServer`]: crate::fs::FileServer
 #[derive(Debug)]
 pub(crate) struct ServerFile {
-    last_modified: Option<String>,
+    last_modified: Option<OffsetDateTime>,
     file: MaybeCompressedFile, 
 }
 
@@ -28,8 +30,8 @@ impl ServerFile {
         let metadata = file.file().metadata().await?;
         let last_modified = metadata.modified()?.duration_since(std::time::UNIX_EPOCH).ok()
             .and_then(|d| i64::try_from(d.as_secs()).ok())
-            .and_then(|sec| OffsetDateTime::from_unix_timestamp(sec).ok())
-            .and_then(|odt| odt.format(&time::format_description::well_known::Rfc2822).ok());
+            .and_then(|sec| OffsetDateTime::from_unix_timestamp(sec).ok());
+            // .and_then(|odt| odt.format(&time::format_description::well_known::Rfc2822).ok());
         
         Ok(Self { last_modified, file })
     }
@@ -38,11 +40,20 @@ impl ServerFile {
 /// Sets the last-modified data for the file response
 impl<'r> Responder<'r, 'static> for ServerFile {
     fn respond_to(self, request: &'r Request<'_>) -> response::Result<'static> {
+        let if_modified_since = request.headers().get_one(IF_MODIFIED_SINCE.as_str())
+            .and_then(|v| time::OffsetDateTime::parse(v, &time::format_description::well_known::Rfc2822).ok());
+
+        match (self.last_modified, if_modified_since) {
+            (Some(lm), Some(ims)) if lm <= ims => 
+                return crate::Response::build().status(Status::NotModified).ok(),
+            _ => {}
+        }
+
         let mut response = self.file.respond_to(request)?;
 
-        if let Some(last_modified) = self.last_modified {
-            response.set_raw_header("last-modified", last_modified);
-        }
+        self.last_modified
+            .and_then(|odt| odt.format(&time::format_description::well_known::Rfc2822).ok())
+            .map(|lm| response.set_raw_header("last-modified", lm));
 
         Ok(response)
     }
